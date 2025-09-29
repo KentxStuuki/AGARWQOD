7b42f77916cfec4589fd98236183daa3ef0251e1
KentXStuuki <mequiambao@outlook.com>
2025-09-29 14:59:21 +0800

    AGARWQOD Bootstrap Connectivity and file structure is done

---
+++ public/src/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Css And Scass Connection -->
+    <link rel="stylesheet" href="../../public/main/style/main.css">
+    <link rel="stylesheet" href="../../public/node_modules/owl.carousel/dist/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="../../public/node_modules/owl.carousel/dist/assets/owl.theme.default.min.css">
+    <link rel="stylesheet" href="../../public/node_modules/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="../../public/node_modules/aos/dist/aos.css">
+
+    <!-- Java Script Connection -->
+    <script src="../../public/main/scripts/bootstrap.bundle.min.js"></script> 
+    <script src="../../public/node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="../../public/node_modules/owl.carousel/dist/owl.carousel.min.js"></script>
+    <script src="../node_modules/aos/dist/aos.js"></script>
+    <title>AGARWQOD | Home</title>
+</head>
+<body>
+    <h1 class="text-primary">Hello World!</h1>
+</body>
+</html>
